f38f5e0ce1b603c168bf85186712db8fcb5a54cd
Wizardy <wizardhassan99@gmail.com>
2024-12-12 17:48:28 +0100

    commit

---
+++ src/Core/Exemples/Exemple1.jsx
@@ -0,0 +1,14 @@
+import { ScrollView } from 'react-native'
+import SimpleView from '../components/SimpleView'
+import SimpleTextView from '../components/SimpleTextView'
+
+function Exemple1() {
+  return (
+    <ScrollView >
+      <SimpleView/>
+      <SimpleTextView/>
+    </ScrollView>
+  )
+};
+
+export default Exemple1
