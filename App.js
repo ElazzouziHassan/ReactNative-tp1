e971ab65bea54546f11bacf11ae713db3e6be27d
Wizardy <wizardhassan99@gmail.com>
2024-12-16 18:03:51 +0100

    commit

--- App.js
+++ App.js
@@ -4,8 +4,16 @@ import SimpleView from './src/Core/components/PART_1/SimpleView';
 import SimpleTextView from './src/Core/components/PART_1/SimpleTextView';
 import SimpleFlatList from './src/Core/components/PART_1/SimpleFlatList';
 import SimpleSectionList from './src/Core/components/PART_1/SimpleSectionList';
+import TestBtn1 from './src/Core/components/testBtn/TestBtn1';
+import TestBtn2 from './src/Core/components/testBtn/TestBtn2';
 
 export default function App() {
+  const handleButtonOnePress = () => {
+    alert("Bouton 1 a été pressé !");
+  };
+  const handleButtonTwoPress = () => {
+    alert("Bouton 2 a été pressé !");
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -13,7 +21,21 @@ export default function App() {
       {/* <SimpleTextView/> */}
       {/* <SimpleScrollView/> */}
       {/* <SimpleFlatList/> */}
-      <SimpleSectionList/>
+      {/* <SimpleSectionList/> */}
+
+      {/* Test sur les bottons */}
+      <TestBtn1 
+          title="Click me 1"
+          onPress={handleButtonOnePress}
+          color="green"
+          textColor="white"  
+      />
+      <TestBtn2 
+          title="Click me 2"
+          onPress={handleButtonTwoPress}
+          color="red"
+          textColor="white"  
+      />
     </SafeAreaView>
   );
 }
